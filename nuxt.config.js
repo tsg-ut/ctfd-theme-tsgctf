24bfc1c2d741491eb92b9155f4809e13196c3d7b
Koki Takahashi <hakatasiloving@gmail.com>
2020-07-07 01:51:49 +0900

    Update OneSignal token to the prod ready one

--- nuxt.config.js
+++ nuxt.config.js
@@ -79,7 +79,7 @@ export default {
 
 	oneSignal: {
 		init: {
-			appId: 'b91908c4-0470-445e-8a5f-8bb886de1cb9',
+			appId: '6acee8f3-7842-4a55-91f6-b63ac21a667d', // public token
 			allowLocalhostAsSecureOrigin: true,
 		},
 	},
--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/sw.js?1593940458540', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/sw.js?1594005960605', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
