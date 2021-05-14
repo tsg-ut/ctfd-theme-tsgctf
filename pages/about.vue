9f259eeb4b7d6e584be20b30ccb35abf5f44f2c4
JP3BGY <6635381+JP3BGY@users.noreply.github.com>
2021-05-14 16:11:56 +0900

    Update about.vue
    delete sponsors
    delete socials
    change contributors

--- pages/about.vue
+++ pages/about.vue
@@ -3,72 +3,28 @@
 		<h2 class="title"><span>About</span></h2>
 		<h3>Period</h3>
 		<p class="period">
-			<a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20200711T16&p1=248" target="_blank" rel="noopener">07/11 07:00</a>
+			<a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20200711T16&p1=248" target="_blank" rel="noopener">05/16 14:33</a>
 			-
-			<a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20200712T16&p1=248" target="_blank" rel="noopener">07/12 07:00</a>
+			<a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20200712T16&p1=248" target="_blank" rel="noopener">05/16 16:13</a>
 		</p>
 		<p :style="{textAlign: 'center'}">(UTC)</p>
-		<h3>Socials</h3>
-		<ul class="socials">
-			<li>
-				<iframe
-					src="https://discord.com/widget?id=546339917459095552&theme=dark"
-					allowtransparency="true"
-					frameborder="0"
-					width="100%"
-					height="100%"
-				>
-					<a href="https://discord.gg/xJn7v62" target="_blank" rel="noopener">Discord</a>
-				</iframe>
-			</li>
-			<li>
-				<timeline
-					id="tsgctf"
-					source-type="profile"
-					:options="{tweetLimit: '3', theme: 'dark', height: '500'}"
-				>
-					<a href="https://twitter.com/tsgctf" target="_blank" rel="noopener">Tweets from @tsgctf</a>
-				</timeline>
-			</li>
-		</ul>
-		<h3>Sponsor</h3>
-		<a href="https://flatt.tech/" target="_blank" rel="noopener">
-			<img src="https://ctf.tsg.ne.jp/images/flatt_image.png" :style="{maxWidth: '60rem'}">
-		</a>
-		<p>Flatt Security Inc. is a cyber security startup based in Tokyo, Japan.</p>
-		<p>We provide security assessments and other cyber security services and have been also developing security software for client companies.</p>
-		<p>We are hiring engineers. Please contact to <a href="mailto:contact@flatt.tech">contact@flatt.tech</a> or <a href="https://flatt.tech/contact">https://flatt.tech/contact</a></p>
-		<h3>Prizes</h3>
-		<ul>
-			<li>1st: 513.31 USD</li>
-			<li>2nd: 213.31 USD</li>
-			<li>3rd: 113.31 USD</li>
-		</ul>
 		<h3>Staffs</h3>
 		<a href="https://tsg.ne.jp/" target="_blank" rel="noopener">
 			<img src="https://tsg.ne.jp/images/tsg-logo.png">
 		</a>
 		<p>
-			TSG CTF is organized by <a href="https://tsg.ne.jp/" target="_blank" rel="noopener">TSG</a>, the official computer society of The
+			TSG LIVE! CTF 6 is organized by <a href="https://tsg.ne.jp/" target="_blank" rel="noopener">TSG</a>, the official computer society of The
 			University of Tokyo.
 		</p>
-		<p>Below is the full list of TSG members who contributed to this TSG CTF in success. So many thanks!!</p>
+		<p>Below is the full list of TSG members who contributed to this CTF in success. So many thanks!!</p>
 		<ul>
 			<li><a href="https://twitter.com/hakatashi" target="_blank" rel="noopener">@hakatashi</a></li>
 			<li><a href="https://twitter.com/moratorium08" target="_blank" rel="noopener">@moratorium08</a></li>
 			<li><a href="https://twitter.com/smallkirby_" target="_blank" rel="noopener">@smallkirby_</a></li>
 			<li><a href="https://twitter.com/naan112358" target="_blank" rel="noopener">@naan112358</a></li>
-			<li><a href="https://twitter.com/satos___jp" target="_blank" rel="noopener">@satos___jp</a></li>
 			<li><a href="https://twitter.com/kcz146" target="_blank" rel="noopener">@kcz146</a></li>
-			<li><a href="https://twitter.com/y0n3uchy" target="_blank" rel="noopener">@y0n3uchy</a></li>
-			<li><a href="https://twitter.com/coil_kpc" target="_blank" rel="noopener">@coil_kpc</a></li>
 			<li><a href="https://twitter.com/JP3BGY" target="_blank" rel="noopener">@JP3BGY</a></li>
-			<li><a href="https://twitter.com/hideo54" target="_blank" rel="noopener">@hideo54</a></li>
-			<li><a href="https://twitter.com/gh_end_" target="_blank" rel="noopener">@gh_end_</a></li>
-			<li><a href="https://twitter.com/sitositositoo" target="_blank" rel="noopener">@sitositositoo</a></li>
-			<li><a href="https://twitter.com/pizzacat83" target="_blank" rel="noopener">@pizzacat83</a></li>
-			<li><a href="https://twitter.com/u6606u5e03" target="_blank" rel="noopener">@u6606u5e03</a></li>
-			<li><a href="https://twitter.com/azaika_" target="_blank" rel="noopener">@azaika_</a></li>
+			<li><a href="https://twitter.com/platypus999" target="_blank" rel="noopener">@platypus999</a></li>
 		</ul>
 	</section>
 </template>
