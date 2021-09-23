bb348c9569af45fa0bf1640660bd7571e918489b
Koki Takahashi <hakatasiloving@gmail.com>
2021-09-23 11:54:01 +0900

    Update build

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1621096352243', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1632365623734', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
