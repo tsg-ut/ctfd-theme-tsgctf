ed4bc57153cb4333d59903cbbe8d4634238e442d
Koki Takahashi <hakatasiloving@gmail.com>
2022-11-19 10:10:14 +0900

    Update pages on about page

--- pages/about.vue
+++ pages/about.vue
@@ -3,9 +3,9 @@
 		<h2 class="title"><span>About</span></h2>
 		<h3>Period</h3>
 		<p class="period">
-			<a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20210516T1433&p1=248" target="_blank" rel="noopener">11/21 14:03</a>
+			<a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20221119T1533&p1=248" target="_blank" rel="noopener">11/19 15:33</a>
 			-
-			<a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20210516T1613&p1=248" target="_blank" rel="noopener">11/21 15:43</a>
+			<a href="https://www.timeanddate.com/worldclock/fixedtime.html?iso=20221119T1713&p1=248" target="_blank" rel="noopener">11/19 17:13</a>
 		</p>
 		<p :style="{textAlign: 'center'}">(JST)</p>
 		<h3>Staffs</h3>
