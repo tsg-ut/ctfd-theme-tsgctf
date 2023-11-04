4e465699cc05a1e8a882a70315eff25c6b3a2a6f
smallkirby <ssmallkirby@gmail.com>
2023-11-04 15:21:55 +0900

    Update build

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1699029336839', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1699078910203', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
