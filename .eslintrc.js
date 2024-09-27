b3587548a09aa6dc8a1c21463c71656e31751a42
nathan <cornelie.nathanpro@gmail.com>
2024-09-27 12:40:33 +0200

    []

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
 		node: true,
 	},
 	parserOptions: {
-		parser: 'babel-eslint',
+		parser: '@babel/eslint-parser',
+		requireConfigFile: false,
 	},
 	extends: ['@nuxtjs', '@hakatashi'],
 	plugins: ['prettier'],
