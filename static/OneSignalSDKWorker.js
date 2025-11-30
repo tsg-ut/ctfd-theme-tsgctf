07955bf5052b4eb1426cd1fa67a5a362bf9dfcaf
iwashiira <iwashiira@gmail.com>
2025-11-30 18:20:03 +0900

    Update build

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1763767069482', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1764494369172', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
