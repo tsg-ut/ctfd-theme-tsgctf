51b17605650fab34557dbb915aaa117288f91a7a
13lackade <13lackade@gmail.com>
2026-04-25 16:42:16 +0900

    Update build

--- static/OneSignalSDKWorker.js
+++ static/OneSignalSDKWorker.js
@@ -1,1 +1,1 @@
-importScripts('/themes/tsgctf/static/sw.js?1777099745124', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
+importScripts('/themes/tsgctf/static/sw.js?1777102919097', 'https://cdn.onesignal.com/sdks/OneSignalSDK.js')
